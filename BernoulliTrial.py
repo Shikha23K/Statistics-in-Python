70935ba99c667e2374317261ce7cb6124442aa1b
SHIKHA K <103320160+Shikha23K@users.noreply.github.com>
2022-04-30 16:58:32 +0530

    Update and rename BernulliTrial.py to BernoulliTrial.py

--- BernoulliTrial.py
+++ BernoulliTrial.py
@@ -1,6 +1,6 @@
-#if coin is biased and given P(H)=0.7,P(T)=0.3 Bernulli Random Variable generated
+#if coin is biased and given P(H)=0.7,P(T)=0.3 Bernoulli Random Variable generated
 import numpy as np
-def BernulliRV(p=.5):
+def BernoulliRV(p=.5):
     X=int(np.random.rand()<=p)
     return X
 countOnes=0
@@ -8,7 +8,7 @@ countZeros=0
 P=0.67122
 n=10000
 for i in range(n):
-    x=BernulliRV(P)
+    x=BernoulliRV(P)
     if x==1:
         countOnes+=1
     else:
